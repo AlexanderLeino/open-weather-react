6211aa65b796875c22accad6a11f786dc086d5a8
Alexander Leino <Alexander.Leino@yahoo.com>
2022-12-28 14:17:04 -0500

    trying another deploy

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(routes)
 
 
 if (process.env.NODE_ENV === 'production') {
-  {  app.use(express.static(path.join(__dirname, 'client/build'))); } //  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
+  app.use(express.static(path.join(__dirname, 'public')));
 }
 
 app.listen(port, () => {
