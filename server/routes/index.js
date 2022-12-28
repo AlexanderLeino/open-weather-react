76fffddbc2e0793a97f35825c3e9f39c170d72b5
Alexander Leino <Alexander.Leino@yahoo.com>
2022-12-28 14:07:00 -0500

    Revert "setting up route on server"
    This reverts commit 782fd63664f93913a0985b362b0c26b232b44331.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const weatherRoutes = require('./api/weather')
 
-router.use('https://open-weather-react-app.herokuapp.com/api', weatherRoutes)
+router.use('/api', weatherRoutes)
 
 module.exports = router
